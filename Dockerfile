e498eac206614f25b0b1da60f1db1bed53a98422
MoBoustta <moboustta6@gmail.com>
2020-01-01 23:12:26 +0100

    ft_server

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,18 @@ EXPOSE 80 443
 RUN apt-get install -y gnupg
 RUN apt-get install -y ufw
 RUN apt-get install -y wget
+RUN apt-get install -y curl
 RUN apt-get install -y expect
 RUN apt-get install -y nginx
 RUN wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
 RUN wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+RUN curl -LO https://wordpress.org/latest.tar.gz
 RUN apt-get -y install php7.3-fpm php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip unzip -y
+RUN tar -xvzf latest.tar.gz -C /var/www/html
+RUN cp -rf /var/www/html/wordpress/* .
+RUN rm -rf wordpress
+RUN chown www-data: /var/www/html/wordpress/ -R
+COPY wp-config-sample.ph /var/www/html
 RUN tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
 RUN mv phpMyAdmin-4.9.0.1-all-languages/ phpMyAdmin
 RUN cp -rf phpMyAdmin ./var/www/html/
@@ -19,4 +26,4 @@ COPY default etc/nginx/sites-available/
 RUN apt-get -y update
 RUN apt-get -y upgrade 
 COPY rc.sh ./
-RUN bash rc.sh
+CMD bash rc.sh
--- rc.sh
+++ rc.sh
@@ -2,7 +2,7 @@
 apt-get install -y lsb-release
 apt-get install -y debconf-utils
 apt-get update
-expect -c " 
+expect -c "
     set timeout 10
     spawn dpkg -i mysql-apt-config_0.8.14-1_all.deb
     expect \"Which MySQL product do you wish to configure?\"
@@ -13,16 +13,24 @@ expect -c "
     send \"4\r\"
     expect EOF
  "
- expect -c " 
-    set timeout 30
-    spawn apt-get install -y mysql-server
-    expect \"Enter root password:\"
-    send \"root\r\"
-    expect \"Re-enter root password: \"
+export DEBIAN_FRONTEND=noninteractive
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get update
+apt-get install -y mysql-server
+service nginx start
+service php7.3-fpm start
+service mysql start
+expect -c "
+    set timeout 10
+    spawn mysql -u root -p
+    expect \"Enter password:\"
     send \"root\r\"
     expect EOF
- "
-apt-get update
-service mysql start
-service php7.3-fpm start
-service nginx start
+"
+CREATE USER 'spirox'@'localhost' IDENTIFIED BY 'spirox';
+GRANT ALL PRIVILEGES ON * . * TO 'spirox'@'localhost';
+FLUSH PRIVILEGES;
+bin/bash
+
